dc8e2552d23f77ecbe74c6700c600f564a8be117
Joe Key <jkey774@gmail.com>
2021-07-15 19:24:15 -0500

    test

--- src/test/java/com/example/demo/ApplicationTest.java
+++ src/test/java/com/example/demo/ApplicationTest.java
@@ -7,20 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class ApplicationTest {
 
-
    @Test
    public void exampleTestSuccess() {
       Assert.assertTrue(true);
    }
-
-//   @Test
-//   public void exampleTestFailure() {
-//      Assert.assertTrue(false);
-//   }
 
 }
