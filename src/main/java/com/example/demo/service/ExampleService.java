189d82a555dc784e81caf4cc73cdcc78fc4f5a00
Joe Key <jkey774@gmail.com>
2021-07-15 19:50:36 -0500

    test

---
+++ src/main/java/com/example/demo/service/ExampleService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleService {
+
+   public String helloWorld() {
+      return "Hello World";
+   }
+
+}
---
+++ src/test/java/com/example/demo/service/ExampleServiceTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class ExampleServiceTest {
+
+//   @Autowired
+//   ExampleService service;
+
+   @Test
+   public void helloWorld() {
+      // String output = service.helloWorld();
+      // Assert.assertEquals("Hello World", output);
+      Assert.assertTrue(true);
+   }
+}
